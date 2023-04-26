88ca68cd195d8830cce3ed87ee691480ccc432b1
Aditya Singh <ee1200461@iitd.ac.in>
2023-04-26 08:22:30 +0000

    setup for Q1

---
+++ buffer_overflow.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "user.h"
+#include "fcntl.h"
+
+void foo() {
+  printf(1, " SECRET_STRING ");
+}
+
+void vulnerable_function(char * input) {
+  char buffer[4];
+  strcpy(buffer, input);
+}
+
+int main(int argc, char ** argv) {
+  int fd = open(" payload ", O_RDONLY);
+  char payload[100];
+  read(fd, payload, 100);
+  vulnerable_function(payload);
+  exit();
+}
+
