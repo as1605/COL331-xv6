99b821f6f63ee01fe9f6063c4af561b156bbaae1
Aditya Singh <ee1200461@iitd.ac.in>
2023-04-28 05:15:29 +0000

    Completed Task 1

--- buffer_overflow.c
+++ buffer_overflow.c
@@ -3,7 +3,7 @@
 #include "fcntl.h"
 
 void foo() {
-  printf(1, " SECRET_STRING ");
+  printf(1, "SECRET_STRING");
 }
 
 void vulnerable_function(char * input) {
@@ -12,7 +12,7 @@ void vulnerable_function(char * input) {
 }
 
 int main(int argc, char ** argv) {
-  int fd = open(" payload ", O_RDONLY);
+  int fd = open("payload", O_RDONLY);
   char payload[100];
   read(fd, payload, 100);
   vulnerable_function(payload);
