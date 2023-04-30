aa6ceb6a4d1fc365c9a52ad29b48535a582640e1
ShreyaGupta8 <78373514+ShreyaGupta8@users.noreply.github.com>
2023-04-30 22:46:39 +0530

    Update proc.h
    offset for memory randomization

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint32_t aslr_offset;        // Offset used for randomizing the memory layout
 };
 
 // Process memory is laid out contiguously, low addresses first:
